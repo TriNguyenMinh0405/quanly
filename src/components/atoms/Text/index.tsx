b81696cef81eb6dfb60c87a3a2fa2cfcd3991ffc
minhtien17495 <minhtien17495@gmail.com>
2020-09-02 22:02:56 +0700

    feat(a-text) implement component

---
+++ src/components/atoms/Text/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+import { customModifier } from '../../../functions';
+interface TextProps {
+    size?: 'fs12' | 'fs14' | 'fs18' | 'fs24';
+    weight?: 'bold';
+}
+const Text: React.FC<TextProps> = ({ children, size, weight }) => {
+    return (
+        <p className={customModifier('a-text', size!, weight!)}>{children}</p>
+    );
+};
+export default Text;
