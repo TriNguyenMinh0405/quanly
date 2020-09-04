a2068514aeb8ac38c40d6edcb792b544eab81522
minhtien17495 <minhtien17495@gmail.com>
2020-09-04 23:48:44 +0700

    feat/a-img

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Icon from './components/atoms/Icon';
 import './App.scss';
 import DashBoard from './components/templates/Dashboard';
+import Img from './components/atoms/Img';
 function App() {
     return (
         <div>
             <Icon modifier="profile" />
             <DashBoard></DashBoard>
+            <Img src="https://i.ytimg.com/vi/p6bj782jntw/maxresdefault.jpg" />
         </div>
     );
 }
