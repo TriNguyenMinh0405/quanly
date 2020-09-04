2e8699c48b1f4ea4be2c0847acebc60c111f0b74
minhtien17495 <minhtien17495@gmail.com>
2020-09-04 23:34:52 +0700

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Icon from './components/atoms/Icon';
+import './App.scss';
+import DashBoard from './components/templates/Dashboard';
 function App() {
     return (
         <div>
             <Icon modifier="profile" />
+            <DashBoard></DashBoard>
         </div>
     );
 }
---
+++ src/components/templates/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+const DashBoard = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-3">shshslk</div>
+                <div className="col-9">hlkhd</div>
+            </div>
+        </div>
+    );
+};
+export default DashBoard;
